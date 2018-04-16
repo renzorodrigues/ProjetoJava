7dd133c12badd83aa7c52c704c6c93edad038713
Renzo Rodrigues <renzors@gmail.com>
2018-04-16 20:22:48 -0300

    Projeto Criado

---
+++ src/main/java/com/renzo/cursoMC/CursoMcApplication.java
@@ -0,0 +1,12 @@
+package com.renzo.cursoMC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoMcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoMcApplication.class, args);
+	}
+}
