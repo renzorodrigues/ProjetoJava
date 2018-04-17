6e7ca00c7e2ceaa3b324cf1f968047387748a2a2
Renzo Rodrigues <renzors@gmail.com>
2018-04-17 12:00:53 -0300

    Criando operação de instanciação

--- src/main/java/com/renzo/cursoMC/CursoMcApplication.java
+++ src/main/java/com/renzo/cursoMC/CursoMcApplication.java
@@ -1,12 +1,31 @@
 package com.renzo.cursoMC;
 
+import java.util.Arrays;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.renzo.cursoMC.domain.Categoria;
+import com.renzo.cursoMC.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CursoMcApplication {
+public class CursoMcApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursoMcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null,"Informática");
+		Categoria cat2 = new Categoria(null,"Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		
 	}
 }
