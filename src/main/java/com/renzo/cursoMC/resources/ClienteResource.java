8dcf7d6dd6715ce7c4fc8240a79b195a5267b56f
Renzo Rodrigues <renzors@gmail.com>
2018-04-23 16:55:43 -0300

    Endpoint /clientes/{id} disponível

---
+++ src/main/java/com/renzo/cursoMC/resources/ClienteResource.java
@@ -0,0 +1,25 @@
+package com.renzo.cursoMC.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.renzo.cursoMC.domain.Cliente;
+import com.renzo.cursoMC.services.ClienteService;
+
+@RestController
+@RequestMapping(value="/clientes")
+public class ClienteResource {
+
+	@Autowired
+	private ClienteService service;
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<?> find(@PathVariable Integer id) {
+		Cliente obj = service.find(id);
+		return ResponseEntity.ok().body(obj);
+	}
+}
---
+++ src/main/java/com/renzo/cursoMC/services/EnderecoService.java
@@ -0,0 +1,24 @@
+package com.renzo.cursoMC.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.renzo.cursoMC.domain.Cliente;
+import com.renzo.cursoMC.repositories.ClienteRepository;
+
+@Service
+public class EnderecoService {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente find(Integer id) {
+		
+		Optional<Cliente> obj = repo.findById(id);		
+		return obj.orElseThrow(() -> new com.renzo.cursoMC.services.exceptions.ObjectNotFoundException(
+				"Objeto não encontrado! ID: " + id + " | Tipo: " + Cliente.class.getName()));
+				
+	}
+}
