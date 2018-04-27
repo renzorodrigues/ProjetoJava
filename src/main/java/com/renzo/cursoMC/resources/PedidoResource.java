78933b8feac0fa2124279483a35af5788dd0e8b4
Renzo Rodrigues <renzors@gmail.com>
2018-04-27 15:19:47 -0300

    Atualizando Categoria com PUT

--- src/main/java/com/renzo/cursoMC/resources/PedidoResource.java
+++ src/main/java/com/renzo/cursoMC/resources/PedidoResource.java
@@ -18,7 +18,7 @@ public class PedidoResource {
 	private PedidoService service;
 	
 	@GetMapping("/{id}")
-	public ResponseEntity<?> find(@PathVariable Integer id) {
+	public ResponseEntity<Pedido> find(@PathVariable Integer id) {
 		Pedido obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
 	}
