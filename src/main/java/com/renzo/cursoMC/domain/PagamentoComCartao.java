e46818f240cba760eebbcbbfbff0d6cd9f5e6697
Renzo Rodrigues <renzors@gmail.com>
2018-05-08 12:30:35 -0300

    Inserindo Pedido

--- src/main/java/com/renzo/cursoMC/domain/PagamentoComCartao.java
+++ src/main/java/com/renzo/cursoMC/domain/PagamentoComCartao.java
@@ -2,9 +2,11 @@ package com.renzo.cursoMC.domain;
 
 import javax.persistence.Entity;
 
+import com.fasterxml.jackson.annotation.JsonTypeName;
 import com.renzo.cursoMC.domain.enums.EstadoPagamento;
 
 @Entity
+@JsonTypeName("pagamentoComCartao")
 public class PagamentoComCartao extends Pagamento {
 	private static final long serialVersionUID = 1L;
 	
--- src/main/java/com/renzo/cursoMC/resources/PedidoResource.java
+++ src/main/java/com/renzo/cursoMC/resources/PedidoResource.java
@@ -1,11 +1,18 @@
 package com.renzo.cursoMC.resources;
 
+import java.net.URI;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.renzo.cursoMC.domain.Pedido;
 import com.renzo.cursoMC.services.PedidoService;
@@ -22,4 +29,12 @@ public class PedidoResource {
 		Pedido obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
 	}
+	
+	@PostMapping
+	public ResponseEntity<Void> insert(@Valid @RequestBody Pedido obj){
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+	
 }
